f44cc9dd781a6008de572005e50fb52b816b0b03
AV <mvav1998@gmail.com>
2026-06-18 16:42:12 +0530

    reverse array

---
+++ array/reverse.java
@@ -0,0 +1,11 @@
+package array;
+
+public class reverse {
+    public static void main(String[] args) {
+        int [] arr={2,5,12,3,7,16,3,9};
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+}
