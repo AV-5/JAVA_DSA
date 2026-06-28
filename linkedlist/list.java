213b9c524b96c196bbdf28ca17de2e234fbda026
AV <mvav1998@gmail.com>
2026-06-29 00:05:37 +0530

    dsa day 8

--- linkedlist/list.java
+++ linkedlist/list.java
@@ -1,7 +1,28 @@
 package linkedlist;
+class Node{
+    int data;
+    Node next;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
 
 public class list {
     public static void main(String[] args) {
         System.out.println("list");
+        Node n1=new Node(10);
+        Node n2=new Node(20);
+        Node n3=new Node(30);
+        n1.next=n2;
+        // n1.next.next=n3;
+        n2.next=n3;
+        Node temp=n1;
+        while(temp!=null){
+            System.out.println(temp.data+" ");
+            temp=temp.next;
+
+        }
+
     }
 }
