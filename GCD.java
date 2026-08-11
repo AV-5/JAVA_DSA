4bc01c11692f48d3101fdf670b07ec62c113c83d
Aryan Vishwakarma <mvav1998@gmail.com>
2026-08-11 17:16:11 +0530

    gcd

---
+++ GCD.java
@@ -0,0 +1,21 @@
+// Problem Statement: Given two integers N1 and N2, find their greatest common divisor.
+
+import java.util.Scanner;
+
+public class GCD {
+    public static int gcd(int a,int b){
+        while(b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    } 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        int a = sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("GCD: "+ gcd(a, b));
+    }
+}
